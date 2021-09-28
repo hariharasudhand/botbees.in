ae2dd5e5a9dc61d752a8fb3dea1bff2adb909771
MalikaWeeroda <malika.k@weeroda.com>
2021-09-28 15:08:02 +0530

    Create UI_Constants.py

---
+++ Project_Retail/UI_Constants.py
@@ -0,0 +1,33 @@
+#URL constants
+LOGIN_URL = "https://exciseportal.py.gov.in/puduvaicalal1/"
+HOME_URL = "https://exciseportal.py.gov.in/puduvaicalal1/home.do"
+
+
+#Xpath Key and Xpath value canstants
+XPATH_USERNAME = "//*[@id=\"txtUName\"]"
+XPATH_PASSWORD = "//*[@id=\"tpass\"]"
+XPATH_CAPTCHA_IMG = "//*[@id=\"img\"]/form/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table/tbody/tr/td/img"
+XPATH_CAPTCHA_TEXT = "//*[@id=\"img\"]/form/table/tbody/tr[5]/td[2]/input"
+XPATH_LOGIN_BTN = "//*[@id=\"button\"]"
+XPATH_PERMITS_LINK = "//*[@id=\"nav\"]/li[4]/a/span"
+XPATH_DEALER_LINK = "//*[@id=\"nav\"]/li[4]/ul/li[4]/a"
+XPATH_DATE_OF_SALE = "//*[@id=\"normalsale\"]/td/fieldset/table/tbody/tr[5]/td[2]/input"
+XPATH_FL2_REATAIL_SALE = "//*[@id=\"normalsale\"]/td/fieldset/table/tbody/tr[5]/td[4]/select"
+XPATH_BRAND_NAME = "//*[@id=\"normalsale\"]/td/fieldset/table/tbody/tr[5]/td[6]/select"
+XPATH_PACK_SIZE = "//*[@id=\"normalsale\"]/td/fieldset/table/tbody/tr[6]/td[2]/select"
+XPATH_EXISTING_STOCK ="//*[@id=\"exstock2\"]/input"
+XPATH_NEW_STOCK ="//*[@id=\"newstock2\"]/input"
+XPATH_NEW_STOCK_PURCHASED ="//*[@id=\"totpurchase2\"]/input"
+XPATH_CLICK = '//*[@id="normalsale"]/td/fieldset/table/tbody/tr[6]/td[6]/input'
+XPATH_LOG_OUT = '//*[@id="nav"]/li[7]/a/span'
+
+
+#BUTTON IMAGE URL
+IMG_OK_BTN = "okBut.png"
+IMG_CAPTCHA_CAPTUREIMAGE_NAME = "cap.png"
+
+#CONTANT VALUES
+USER_NAME = "MUNIYANDI"
+PASSWORD = "kannanSMW@272"
+LOGIN_RETRY = 2
+FL2_REATAIL_SALE = 'FL2 REATAIL SALE'
