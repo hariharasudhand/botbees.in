b3f079851cba33cf16364f229b8ab7f3073dac1a
MalikaWeeroda <malika.k@weeroda.com>
2021-09-28 15:09:00 +0530

    Create Mapping_loader.py

---
+++ Project_Retail/Mapping_loader.py
@@ -0,0 +1,60 @@
+# This class loads the mapping file, which has customer side
+# ERP Product name mapped to the equavalent name in Govt Portal
+from numpy import maximum
+import pandas as pd
+from pandas.core.indexes.base import Index
+
+
+class MappingFileLoader:
+    data = None
+    data1 = None
+    data2 = None
+
+    def __init__(self):
+        global data,data1,data2        
+        xls = pd.ExcelFile('D:\SMWSED\config\mapping_file.xlsx')
+        xls2 = pd.ExcelFile('D:\SMWSED\config\MAPPING LIQUOR NAME new.xlsx')
+        
+        data = xls2.parse(xls2.sheet_names[0])
+        data1 = xls.parse(xls.sheet_names[1])
+        data2 = xls.parse(xls.sheet_names[0])
+
+        
+    @staticmethod
+    def fetch_Product_Mapping():
+        global data
+        mappedData = []
+        df = pd.DataFrame(data)
+        for index, row in df.iterrows():
+            rowData = {row['BRAND']: row['UI_NAME']}
+            mappedData.append(rowData)
+            # print(rowData)
+        return mappedData
+
+    @staticmethod    
+    def fetch_Parse_param_mapping():
+        global data1
+        mappedData1 = []
+        df1 = pd.DataFrame(data1)
+        for index, row in df1.iterrows():
+            rowData1 = {row['param.name']: row['param.value']}
+            mappedData1.append(rowData1)
+            #print(rowData1)
+        return mappedData1
+
+    @staticmethod
+    def fetch_Product_Mapping2():
+        global data2
+        mappedData2 = []
+        df2 = pd.DataFrame(data2)
+        for index, row in df2.iterrows():
+            rowData2 = {row['BRAND']: row['UI_NAME']}
+            mappedData2.append(rowData2)
+            # print(rowData)
+        return mappedData2    
+        
+        
+
+mfl = MappingFileLoader()
+mfl.fetch_Product_Mapping()
+mfl.fetch_Parse_param_mapping()
